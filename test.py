1c2599c177e2ec0454045e22ca17fe5bb3314732
adam-cie <87044575+adam-cie@users.noreply.github.com>
2022-11-08 04:30:40 +0100

    Update test.py

--- test.py
+++ test.py
@@ -11,4 +11,4 @@ objectives = np.array(('max','max','min','max','min','min','min','max'))
 
 buses = MSD_Transformer(data, criteria, alternatives, None, objectives, None, 'I')
 buses.transform()
-assert (buses.ranked_alternatives == np.array(['b24', 'b26', 'b07', 'b16', 'b18', 'b25', 'b04', 'b01', 'b28', 'b09', 'b02', 'b13', 'b11', 'b32', 'b21', 'b12', 'b27', 'b17', 'b06', 'b29', 'b20', 'b14', 'b23', 'b19', 'b03', 'b30', 'b08', 'b22', 'b15', 'b10', 'b31', 'b05'])).all(), "wrong ranking for bus database"
+assert (buses.ranked_alternatives == np.array(['b24', '26', 'b07', 'b16', 'b18', 'b25', 'b04', 'b01', 'b28', 'b09', 'b02', 'b13', 'b11', 'b32', 'b21', 'b12', 'b27', 'b17', 'b06', 'b29', 'b20', 'b14', 'b23', 'b19', 'b03', 'b30', 'b08', 'b22', 'b15', 'b10', 'b31', 'b05'])).all(), "wrong ranking for bus database"
