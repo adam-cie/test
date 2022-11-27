1f8c08beca63647edaa4745f120ef641123169aa
adam-cie <87044575+adam-cie@users.noreply.github.com>
2022-11-27 19:51:07 +0100

    Add files via upload

--- tests.py
+++ tests.py
@@ -11,4 +11,18 @@ def test_mytest():
 
     TopsisTransformer = MSDTransformer()
     with pytest.raises(Exception):
-        TopsisTransformer.fit(1)
+        TopsisTransformer.transform()
+
+
+class TestClass:
+    def test_one(self):
+        TopsisTransformer = MSDTransformer()
+        with pytest.raises(Exception):
+            TopsisTransformer.fit(1)
+
+    def test_two(self):
+        TopsisTransformer = MSDTransformer()
+        df = pd.read_csv("bus.csv", sep = ';', index_col = 0)
+
+        with pytest.raises(Exception):
+            TopsisTransformer.fit(df,2)
