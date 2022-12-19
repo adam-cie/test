abd526a85187693ed67bc5740021f29a35355b40
adam-cie <87044575+adam-cie@users.noreply.github.com>
2022-12-19 20:19:41 +0100

    Add files via upload

---
+++ test_wrong_parameters.py
@@ -0,0 +1,14 @@
+from main_3 import MSDTransformer
+import numpy as np
+import pandas as pd
+import pytest
+
+class TestParameters:
+    df = pd.read_csv("bus.csv", sep = ';', index_col = 0)
+    objectives = ['max','max','min','max','min','min','min','max']
+
+    def test_single_objective(self):
+        for i in ['c', 'g', 'min', 'max', 'gain', 'cost']:
+            buses = MSDTransformer()
+            buses.fit(self.df, objectives = i)
+            buses.transform()
